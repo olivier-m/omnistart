5f3720ee691c1c6c74dde47789f9feaaabacb9f4
Olivier Meunier <olivier@neokraft.net>
2017-11-29 21:44:40 +0100

    Store favicon in png with 64px max dimension

--- src/lib/favicon.js
+++ src/lib/favicon.js
@@ -6,11 +6,13 @@ const request = axios.create({
   'method': 'GET',
   'timeout': 8000,
 });
+
 request.interceptors.request.use(config => {
   // Prevent HTTP basic authentication popup
   config.headers['Authorization'] = '--ignore--';
   return config;
 });
+
 
 const linkTypes = [
   'link[rel="shortcut icon"]',
@@ -19,11 +21,57 @@ const linkTypes = [
   'link[rel="apple-touch-icon-precomposed"]'
 ];
 
-const imageEncode = function(arrayBuffer) {
-  return btoa([].reduce.call(
-    new Uint8Array(arrayBuffer), (p, c) => p + String.fromCharCode(c), '')
-  );
+
+const bufferToCanvas = function(data, mimetype) {
+  let blob = new Blob([data], {type: mimetype});
+  let imgURL = URL.createObjectURL(blob);
+  let img = new Image();
+
+  return new Promise((resolve, reject) => {
+    img.addEventListener('load', function() {
+      URL.revokeObjectURL(imgURL);
+      let canvas = document.createElement('canvas');
+      canvas.width = this.width;
+      canvas.height = this.height;
+      let ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0);
+      resolve(canvas);
+    });
+
+    img.addEventListener('error', function(event) {
+      reject(event);
+    });
+
+    img.src = imgURL;
+  });
 };
+
+
+const resizeCanvas = function(original, maxSize) {
+  let w = original.width;
+  let h = original.height;
+  let ratio = h/w;
+
+  if (ratio >= 1) {
+    h = Math.min(maxSize, h);
+    w = Math.round(h / ratio);
+  }
+  else {
+    w = Math.min(maxSize, w);
+    h = Math.round(w * ratio);
+  }
+
+  let canvas = document.createElement('canvas');
+  canvas.width = w;
+  canvas.height = h;
+  let ctx = canvas.getContext('2d');
+  ctx.drawImage(original,
+    0, 0, original.width, original.height,
+    0, 0, canvas.width, canvas.height
+  );
+  return canvas;
+};
+
 
 const findFaviconInResponse = function(response) {
   let nodes = response.data.querySelectorAll(linkTypes.join(','));
@@ -59,44 +107,52 @@ const findFaviconInResponse = function(response) {
 };
 
 
-const fetchIcon = function(href) {
-  return request.get(href, {
+const fetchIconCanvas = async function(href) {
+  let r = await request.get(href, {
     'responseType': 'arraybuffer'
-  }).then(r => {
-    let encoded = imageEncode(r.data);
-    let mimetype = r.headers['content-type'];
-    let href = r.request.responseURL;
-
-    if (href.match(/\.ico$/)) {
-      mimetype = 'image/vnd.microsoft.icon';
-    }
-
-    if (mimetype.indexOf('image/') !== 0) {
-      throw new Error(`${href} is not an image`);
-    }
-    return `data:${mimetype};base64,${encoded}`;
   });
+
+  let mimetype = r.headers['content-type'];
+  href = r.request.responseURL;
+
+  if (href.match(/\.ico$/)) {
+    mimetype = 'image/vnd.microsoft.icon';
+  }
+
+  if (mimetype.indexOf('image/') !== 0) {
+    throw new Error(`${href} is not an image`);
+  }
+
+  return await bufferToCanvas(r.data, mimetype);
 };
 
-export const fetchFavicon = function(url) {
-  return request.get(url, {
-    'responseType': 'document'
-  }).then(findFaviconInResponse)
-    .catch(() => {
-      throw new Error('No favicon found');
-    }).then(icons => {
-      let icon = icons.shift();
 
-      function loop(i) {
-        if (typeof i === 'undefined') {
-          return Promise.reject('no icon');
-        }
-        return fetchIcon(i.href).catch(() => {
-          icon = icons.shift();
-          return loop(icon);
-        });
+export const fetchFavicon = async function(url) {
+  try {
+    let r = await request.get(url, {
+      'responseType': 'document'
+    });
+    let icons = findFaviconInResponse(r);
+    let icon = icons.shift();
+
+    let loop = async function(i) {
+      if (typeof i === 'undefined') {
+        throw new Error('no icon');
       }
 
-      return loop(icon);
-    });
+      try {
+        let canvas = await fetchIconCanvas(i.href);
+        return resizeCanvas(canvas, 64).toDataURL('image/png');
+      }
+      catch(e) {
+        icon = icons.shift();
+        return await loop(icon);
+      }
+    };
+
+    return await loop(icon);
+  }
+  catch(e) {
+    throw new Error('No favicon found');
+  }
 };
